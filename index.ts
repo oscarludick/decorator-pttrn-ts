8449e826e2f235f792009a25714fe23775ddb2f8
oscarludick <oscarludick@gmail.com>
2021-01-13 12:34:35 -0600

    1 file modified, 7 added, 7 deleted

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import { HeroWithHelm } from "./hear-gear/gear/hero-helm";
-import { HeroWithShield } from "./hear-gear/gear/hero-shield";
-import { HeroWithSword } from "./hear-gear/gear/hero-sword";
+import { HeroWithHelm } from "./hero-gear/gear/hero-helm";
+import { HeroWithShield } from "./hero-gear/gear/hero-shield";
+import { HeroWithSword } from "./hero-gear/gear/hero-sword";
 
-import { RangerHero } from "./hear-gear/heroes/ranger-hero";
-import { WarriorHero } from "./hear-gear/heroes/warrior-hero";
+import { RangerHero } from "./hero-gear/heroes/ranger-hero";
+import { WarriorHero } from "./hero-gear/heroes/warrior-hero";
 
-import { Hero } from "./hear-gear/hero";
+import { Hero } from "./hero-gear/hero";
 
 const hero1: Hero = new HeroWithSword(new HeroWithShield(new WarriorHero()));
 console.log(`${hero1.getGear()} , Total: ${hero1.powerup()}`);
